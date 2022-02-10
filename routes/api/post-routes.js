91b0b4b2570e00decacbefe961af6ec412f423b0
Tammy Fina <finatammy@gmail.com>
2022-02-10 10:03:27 -0500

    post put, delete and get order DESC

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -12,6 +12,7 @@ router.get('/', (req, res) => {
             'title', 
             'created_at'
         ],
+        order: [['created_at', 'DESC']], 
         include: [
             {
             model: User,
@@ -80,5 +81,47 @@ router.post('/',  (req, res) => {
     });
 });
 
+router.put('/:id', (req, res) => {
+    Post.update(
+        {
+        title: req.body.title
+        },
+        {
+        where: {
+            id: req.params.id
+        }
+    }
+    )
+        .then(dbPostData => {
+            if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+        res.json(dbPostData);
+    })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+    });
+});
+
+router.delete('/:id', (req, res) => {
+    Post.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+        res.json(dbPostData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+    });
+});
 
 module.exports = router;
