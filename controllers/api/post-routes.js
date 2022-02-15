62f5f437aefe83e4f68c8907d7e4c2cd5a0320e4
Cheri M <cmerrym1@kent.edu>
2022-02-15 17:20:33 -0500

    DONT MERGE THIS, pushing so I can pull

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -45,7 +45,6 @@ router.get('/:id', (req, res) => {
             'post_url',
             'title',
             'created_at',
-        // [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
         ],
         include: [
         {
@@ -131,18 +130,5 @@ router.delete('/:id', (req, res) => {
             res.status(500).json(err);
     });
 });
-
-router.put('/upvote', (req, res) => {
-    // make sure the session exists first
-    if (req.session) {
-      // pass session id along with all destructured properties on req.body
-      Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
-        .then(updatedVoteData => res.json(updatedVoteData))
-        .catch(err => {
-          console.log(err);
-          res.status(500).json(err);
-        });
-    }
-  });
 
 module.exports = router;
