a3aad0a7ac81b0ba40f5903a9239a53fe004c431
Tammy Fina <finatammy@gmail.com>
2022-02-19 12:22:30 -0500

    seeding db test

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -2,21 +2,21 @@ const { Comment } = require('../models');
 const chalk = require('chalk');
 
 const commentdata = [
-  {
-    comment_text: 'Love this place!',
-    user_id: 2,
-    post_id: 1
-  },
-  {
-    comment_text: 'Cant wait to go here!',
-    user_id: 1,
-    post_id: 2
-  },
-  {
-    comment_text: 'Adding this to my roadtrip list!',
-    user_id: 1,
-    post_id: 3
-  },
+  // {
+  //   comment_text: 'Love this place!',
+  //   user_id: 2,
+  //   post_id: 1
+  // },
+  // {
+  //   comment_text: 'Cant wait to go here!',
+  //   user_id: 1,
+  //   post_id: 2
+  // },
+  // {
+  //   comment_text: 'Adding this to my roadtrip list!',
+  //   user_id: 1,
+  //   post_id: 3
+  // },
   {
     comment_text: 'Sooo retro!',
     user_id: 3,
